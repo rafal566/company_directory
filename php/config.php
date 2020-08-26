bf540cc4a01f3e2c9ca4f088102437c3e999dcb6
rafal566 <60554900+rafal566@users.noreply.github.com>
2020-08-26 16:04:34 +0100

    Add files via upload

--- php/config.php
+++ php/config.php
@@ -1,12 +1,10 @@
 <?php
 
-$url = parse_url(getenv("mysql://b7de7cd547a86f:bf7c975c@us-cdbr-east-02.cleardb.com/heroku_27e421d583545ae?reconnect=true"));
-
-$cd_host = $url["us-cdbr-east-02.cleardb.com"];
-$cd_port = 3306;
-$cd_socket = "";
-$cd_user = $url["b7de7cd547a86f"];
-$cd_password = $url["bf7c975c"];
-$cd_dbname = substr($url["heroku_27e421d583545ae"], 1);
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+	$cd_user = "root";
+	$cd_password = "sElling@348";
+	$cd_dbname = "companydirectory";
 
 ?>
