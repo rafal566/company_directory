15d10d9f16f735e9ba3e135139b48d8bb5477026
rafal566 <60554900+rafal566@users.noreply.github.com>
2020-09-06 21:32:47 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,11 @@
 <?php
 
-	$cd_host = "127.0.0.1";
+	$cd_host = "198.71.227.89";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root";
-	$cd_password = "sElling@348";
-	$cd_dbname = "companydirectory";
+	$cd_user = "rafalbury";
+	$cd_password = "Gbyy40&7";
+	$cd_dbname = "ph16295073537_";
 
 ?>
+
